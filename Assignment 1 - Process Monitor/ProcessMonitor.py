188a171848221208c008c9b6dc90c4f64f425644
Dvir Barzilay <dvir.barzilay@gmail.com>
2018-04-16 20:19:22 +0300

    update gitignore file for hidden personal information files. Additionally, append  the ability to write the processList.txt file with all the samples

--- Assignment 1 - Process Monitor/ProcessMonitor.py
+++ Assignment 1 - Process Monitor/ProcessMonitor.py
@@ -5,7 +5,7 @@ import sys
 
 class ProcessMonitor:
     """
-    This class contain the main functional for the process monitor.
+    This class contain the main functional of the process monitor.
     """
     __delay = 0
     __processListFile = None
@@ -15,19 +15,17 @@ class ProcessMonitor:
         self.__delay = delay
 
     def start_monitor(self):
-        self.__processListFile = open('processList.txt', 'w')
-        self.__statusLogFile = open('Status_Log.txt', 'w')
-        prods = psutil.process_iter()
-        for prodc in prods:
-            print(prodc.name)
-
-    def close(self):
-        if not self.__processListFile:
+        while 1:
+            self.__processListFile = open('processList.txt', 'a')
+            processes = psutil.process_iter()
+            self.__processListFile.write('-'*100 + '\n')
+            self.__processListFile.write(time.ctime() + '\n\n')
+            for proc in processes:
+                name = str(proc.name)
+                self.__processListFile.write(name + '\n')
+            self.__processListFile.write('\n\n')
             self.__processListFile.close()
-        if not self.__statusLogFile:
-            self.__statusLogFile.close()
+            time.sleep(self.__delay)
 
-
-pm = ProcessMonitor(1)
+pm = ProcessMonitor(10)
 pm.start_monitor()
-pm.close()
