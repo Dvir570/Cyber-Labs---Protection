073beae5b361e06c92dbee5b043ac30f9889e9f0
Dvir Barzilay <dvir.barzilay@gmail.com>
2018-04-18 00:34:57 +0300

    finish the main logic of the Process Monitor whith writing log files

--- Assignment 1 - Process Monitor/ProcessMonitor.py
+++ Assignment 1 - Process Monitor/ProcessMonitor.py
@@ -1,6 +1,6 @@
-import os
 import psutil
 import time
+from copy import deepcopy
 import sys
 
 class ProcessMonitor:
@@ -8,24 +8,53 @@ class ProcessMonitor:
     This class contain the main functional of the process monitor.
     """
     __delay = 0
-    __processListFile = None
-    __statusLogFile = None
+    __prevSample = []
+    __prevPids = []
 
     def __init__(self, delay):
         self.__delay = delay
 
     def start_monitor(self):
         while 1:
-            self.__processListFile = open('processList.txt', 'a')
-            processes = psutil.process_iter()
-            self.__processListFile.write('-'*100 + '\n')
-            self.__processListFile.write(time.ctime() + '\n\n')
-            for proc in processes:
-                name = str(proc.name)
-                self.__processListFile.write(name + '\n')
-            self.__processListFile.write('\n\n')
-            self.__processListFile.close()
+            current_time = time.ctime()
+            process_list_file = open('processList.txt', 'a')
+            current_psutil = psutil
+            current_pids = current_psutil.pids()
+            process_list_file.write('-'*100 + '\n')
+            process_list_file.write(current_time + '\n\n')
+            for proc in current_psutil.process_iter():
+                process_list_file.write(str(proc.name) + '\n')
+            #gone, alive = psutil.wait_procs(processes, timeout=10, callback=None)
+            new_processes = []
+            killed_processes = []
+            for pid in self.__prevPids:
+                if pid not in current_pids:
+                    killed_processes.append(pid)
+            for pid in current_pids:
+                if pid not in self.__prevPids:
+                    new_processes.append(pid)
+
+            process_list_file.write('\n\n')
+            process_list_file.close()
+            self.__prevPids = current_pids
+            status_log_file = open('Status_Log.txt', 'a')
+            status_log_file.write('-' * 100 + '\n')
+            status_log_file.write(current_time + '\n\n')
+            print('New running processes:' + '\n')
+            status_log_file.write('New running processes:' + '\n')
+            for pid in new_processes:
+                print(str(pid) + '\n')
+                status_log_file.write(str(pid) + '\n')
+            print('\n' + 'Killed processes:' + '\n')
+            status_log_file.write('\n' + 'Killed processes:' + '\n')
+            for pid in killed_processes:
+                print(str(pid) + '\n')
+                status_log_file.write(str(pid) + '\n')
+            print('\n\n')
+            status_log_file.write('\n\n')
+            status_log_file.close()
             time.sleep(self.__delay)
+
 
 pm = ProcessMonitor(10)
 pm.start_monitor()
