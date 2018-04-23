165f2c07c1c2718e7f952ba0b596689f9f8fae0e
Dvir Barzilay <dvir.barzilay@gmail.com>
2018-04-23 19:39:51 +0300

    Reorganization files with archicterture

--- Assignment 1 - Process Monitor/ProcessMonitor.py
+++ Assignment 1 - Process Monitor/ProcessMonitor.py
@@ -1,66 +1,33 @@
 import psutil
 import time
-import os
-from stat import S_IREAD, S_IRGRP, S_IROTH, S_IWUSR
-
+import zipfile
+import WriteProcessListFile as plist
+import WriteProcessLogFile as plog
 
 class ProcessMonitor:
     """
     This class contain the main functional of the process monitor.
     """
     __delay = 0
-    __prevSample = []
-    __prevPids = []
+    __prev_pids = []
 
     def __init__(self, delay):
         self.__delay = delay
 
     def start_monitor(self):
+        zipf = zipfile.ZipFile('zipfile', 'w', zipfile.ZIP_DEFLATED)
+        for root, dirs, files in os.walk(path):
+            for file in files:
+                zipf.write(os.path.join(root, file))
+        zipf.close()
         while 1:
             current_time = time.ctime()
-            if os.path.isfile('processList.txt'):
-                os.chmod('processList.txt', S_IWUSR | S_IREAD)
-            process_list_file = open('processList.txt', 'a')
             current_psutil = psutil
             current_pids = current_psutil.pids()
-            process_list_file.write('-'*100 + '\n')
-            process_list_file.write(current_time + '\n\n')
-            for proc in current_psutil.process_iter():
-                process_list_file.write(str(proc.name) + '\n')
-            new_processes = []
-            killed_processes = []
-            for pid in self.__prevPids:
-                if pid not in current_pids:
-                    killed_processes.append(pid)
-            for pid in current_pids:
-                if pid not in self.__prevPids:
-                    new_processes.append(pid)
-
-            process_list_file.write('\n\n')
-            process_list_file.close()
-            os.chmod('processList.txt', S_IREAD | S_IRGRP | S_IROTH)
-            self.__prevPids = current_pids
-            if os.path.isfile('Status_Log.txt'):
-                os.chmod('Status_Log.txt', S_IWUSR | S_IREAD)
-            status_log_file = open('Status_Log.txt', 'a')
-            status_log_file.write('-' * 100 + '\n')
-            status_log_file.write(current_time + '\n\n')
-            print('New running processes:' + '\n')
-            status_log_file.write('New running processes:' + '\n')
-            for pid in new_processes:
-                print(str(pid))
-                status_log_file.write(str(pid) + '\n')
-            print('\n' + 'Killed processes:' + '\n')
-            status_log_file.write('\n' + 'Killed processes:' + '\n')
-            for pid in killed_processes:
-                print(str(pid))
-                status_log_file.write(str(pid) + '\n')
-            print('\n\n')
-            status_log_file.write('\n\n')
-            status_log_file.close()
-            os.chmod('Status_Log.txt', S_IREAD | S_IRGRP | S_IROTH)
+            plist.write_process_list(current_psutil, current_time)
+            self.__prev_pids = plog.write_process_log(current_pids, self.__prev_pids, current_time)
+            #pyminizip.compress_multiple(['processList.txt', 'Status_Log.txt'], "file.zip", "1233", 4)
             time.sleep(self.__delay)
-
 
 pm = ProcessMonitor(10)
 pm.start_monitor()
