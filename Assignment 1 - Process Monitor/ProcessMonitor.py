8ab350bf775ab236b33eaa1d2f814eee5ad361cb
Dvir Barzilay <dvir.barzilay@gmail.com>
2018-04-23 20:48:46 +0300

    Refresh code

--- Assignment 1 - Process Monitor/ProcessMonitor.py
+++ Assignment 1 - Process Monitor/ProcessMonitor.py
@@ -1,6 +1,5 @@
 import psutil
 import time
-import zipfile
 import WriteProcessListFile as plist
 import WriteProcessLogFile as plog
 
@@ -15,18 +14,12 @@ class ProcessMonitor:
         self.__delay = delay
 
     def start_monitor(self):
-        zipf = zipfile.ZipFile('zipfile', 'w', zipfile.ZIP_DEFLATED)
-        for root, dirs, files in os.walk(path):
-            for file in files:
-                zipf.write(os.path.join(root, file))
-        zipf.close()
         while 1:
             current_time = time.ctime()
             current_psutil = psutil
             current_pids = current_psutil.pids()
             plist.write_process_list(current_psutil, current_time)
             self.__prev_pids = plog.write_process_log(current_pids, self.__prev_pids, current_time)
-            #pyminizip.compress_multiple(['processList.txt', 'Status_Log.txt'], "file.zip", "1233", 4)
             time.sleep(self.__delay)
 
 pm = ProcessMonitor(10)
