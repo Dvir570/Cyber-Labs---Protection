5b5abc1adaa66396dbf14a7116f464a5ee594762
Dvir Barzilay <dvir.barzilay@gmail.com>
2018-05-01 09:56:31 +0300

    Watchdog is watching on modified files. Bug: watchdog reports about monitor edit

---
+++ Assignment 1 and 2 - Process Monitor/PausingObserver.py
@@ -0,0 +1,23 @@
+import time
+import contextlib
+import watchdog.observers
+
+
+class PausingObserver(watchdog.observers.Observer):
+    def dispatch_events(self, *args, **kwargs):
+        if not getattr(self, '_is_paused', False):
+            super(PausingObserver, self).dispatch_events(*args, **kwargs)
+
+    def pause(self):
+        self._is_paused = True
+
+    def resume(self):
+        time.sleep(self.timeout)  # allow interim events to be queued
+        self.event_queue.queue.clear()
+        self._is_paused = False
+
+    @contextlib.contextmanager
+    def ignore_events(self):
+        self.pause()
+        yield
+        self.resume()
