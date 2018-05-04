1a81073a4215183e0275c616110987e8cdd725b4
Dvir Barzilay <dvir.barzilay@gmail.com>
2018-05-04 09:40:01 +0300

    Input validation

--- Assignment 1 and 2 - Process Monitor/ProcessMonitor.py
+++ Assignment 1 and 2 - Process Monitor/ProcessMonitor.py
@@ -37,18 +37,20 @@ class ProcessMonitor:
         prior_date = None
         after_date = None
         while not isValid:
+            flag = False
             prior_date = raw_input("Type Date of the prior sample (yy-mm-dd hh-mm): ")
             try:  # strptime throws an exception if the input doesn't match the pattern
                 datetime.datetime.strptime(prior_date, "%y-%m-%d %H-%M")
-                isValid = True
+                flag = True
             except:
                 print "Input doesn't match the pattern, try again!\n"
-        isValid = False
-        while not isValid:
             after_date = raw_input("Type Date of the after sample (yy-mm-dd hh-mm): ")
             try:  # strptime throws an exception if the input doesn't match the pattern
                 datetime.datetime.strptime(after_date, "%y-%m-%d %H-%M")
-                isValid = True
+                if flag and prior_date < after_date:
+                    isValid = True
+                else:
+                    print "You have at first to input the prior date!\n"
             except:
                 print "Input doesn't match the pattern, try again!\n"
         prior_sample = self.__plist.get_sample_by_date(prior_date)
@@ -64,13 +66,19 @@ class ProcessMonitor:
 
 again = True
 while again:
-    choose = input('press 1 to start monitor\npress 2 to see differences between two dates\n')
-    if choose == 1:
-        delay = input('Enter a time in seconds to sleep after samling: ')
-        pm = ProcessMonitor()
-        thread.start_new_thread(pm.start_monitor(delay))
-        again = False
-        thread.join()
-    elif choose == 2:
-        pm = ProcessMonitor()
-        pm.samples_diff()
+    while 1:
+        choose = input('press 1 to start monitor\npress 2 to see differences between two dates\npress 3 to exit\n')
+
+        if choose == 1:
+            delay = input('Enter a time in seconds to sleep after samling: ')
+            pm = ProcessMonitor()
+            thread.start_new_thread(pm.start_monitor(delay))
+            again = False
+            thread.join()
+        elif choose == 2:
+            pm = ProcessMonitor()
+            pm.samples_diff()
+        elif choose == 3:
+            exit(0)
+        else:
+            print "Invalid input!\n"
